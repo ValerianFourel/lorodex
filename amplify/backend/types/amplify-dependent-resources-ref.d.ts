b7b7ee07573552ad47bcc6f69c56865778235e64
Valerian Fourel <77210325+ValerianFourel@users.noreply.github.com>
2025-08-29 21:25:15 +0200

    Initial push of local repo

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "lorodex20775a6c": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
